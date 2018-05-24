918da8025c2d181ea65de07b640e5428b6f2cfed
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2018-05-24 16:55:29 +0800

    Add config

---
+++ config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+  "io/ioutil"
+  "gopkg.in/yaml.v2"
+  "os"
+)
+
+type GameSetting struct {
+  Hp int `yaml: hp`
+}
+
+var GameSettingConfig GameSetting
+var GO_ENV string
+
+var VALID_ENV map[string]bool = map[string]bool{
+  "dev": true,
+}
+
+func init() {
+  GO_ENV := os.Getenv(GO_ENV)
+  if _, ok := VALID_ENV[GO_ENV]; !ok {
+    GO_ENV = "dev"
+  }
+  parseYmlConfigByEnv(GO_ENV, &GameSettingConfig)
+}
+
+// 项目根目录路径
+//
+func parseYmlConfigByEnv(env string, configPtr *GameSetting) {
+  path := "config/" + env + ".yml"
+  content, err := ioutil.ReadFile(path)
+  if err != nil {
+    panic("missing config file");
+  }
+  err = yaml.Unmarshal(content, configPtr)
+  if err != nil {
+    panic("parse config file failed");
+  }
+  return;
+}
+
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+  "hangmango/config"
+)
+
+func main() {
+  fmt.Println(config.GameSettingConfig.Hp)
+}
